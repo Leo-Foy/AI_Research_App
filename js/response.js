55302312d0be3c8787552666dbe60ee7fb306b4a
LeoFoy <foyl1@nku.edu>
2024-07-29 05:45:55 -0400

    added landing page promt function
    also changed some css font and margins

--- js/response.js
+++ js/response.js
@@ -21,6 +21,25 @@ function post() {
     respond();
 }
 
+function postPromt(promt) {
+    var value = promt.textContent;
+    var chatDiv = document.getElementById("messages");
+    var message = document.createElement("li");
+    const img = document.createElement("img");
+    img.src = "Assets/penguin.jpg";
+    img.classList.add("avatar");
+
+    message.setAttribute("class", "bubble sender");
+
+    message.appendChild(document.createTextNode(value));
+    chatDiv.appendChild(message);
+    message.appendChild(img);
+
+    btn.disabled = true;
+
+    respond();
+}
+
 function respond() {
     let value = "Great! Let's get started.";
     var chatDiv = document.getElementById("messages");
@@ -28,7 +47,7 @@ function respond() {
     let index = 0;
     var message = document.createElement("li");
     const img = document.createElement("img");
-        img.src = "Assets/penguin.jpg";
+        img.src = "Assets/AI_Logo.jpg";
         img.classList.add("avatar");
     message.setAttribute("class", "bubble receiver");
     message.appendChild(img);
